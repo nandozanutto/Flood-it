0bf1b074177a727487e3b039f901768db79b828b
nandozanutto <nando.zanutto@gmail.com>
2021-07-30 13:30:40 -0300

    graph struct changed to matrix

--- graph.c
+++ graph.c
@@ -1,114 +1,140 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/* REPRESENTAÇÃO POR LISTAS DE ADJACÊNCIA: A estrutura graph representa um grafo. O campo adj é um ponteiro para o vetor de listas de adjacência, o campo V contém o número de vértices e o campo A contém o número de arcos do grafo. */
-
 typedef struct graph *Graph;
-typedef struct vertex *Vertex;
-typedef struct node *link;
+typedef struct vertex *ApontaVertex;
+
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A estrutura graph representa um grafo. O campo adj é um ponteiro para a matriz de adjacências do grafo. O campo V contém o número de vértices e o campo A contém o número de arcos do grafo. */
+
+struct graph {
+   int V; 
+   int A; 
+   int **adj; 
+};
 
 
 struct vertex{
     int numVertice;
     int cor;
-    link primeiroVizinho;
 };
 
-struct graph {
-   int V; 
-   int A; 
-   Vertex *vertices;//vetor de ponteiros para vértices 
-};
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHinit() constrói um grafo com vértices 0 1 .. V-1 e nenhum arco. */
+static int **MATRIXint( int r, int c, int val) { 
+   int **m = malloc( r * sizeof (int *));
+   for (int i = 0; i < r; ++i) 
+      m[i] = malloc( c * sizeof (int));
+   for (int i = 0; i < r; ++i)
+      for (int j = 0; j < c; ++j)
+         m[i][j] = val;
+   return m;
+}
 
-/* Um Graph é um ponteiro para um graph. */
-
-
-/* A lista de adjacência de um vértice v é composta por nós do tipo node. Cada nó da lista corresponde a um arco e contém um vizinho w de v e o endereço do nó seguinte da lista. Um link é um ponteiro para um node. */
-
-struct node { 
-   Vertex w; 
-   link next; 
-};
-
-Graph criaGrafo( int V) { 
+Graph GRAPHinit( int V) { 
    Graph G = malloc( sizeof *G);
    G->V = V; 
    G->A = 0;
-   G->vertices = malloc( V * sizeof (Vertex));
-   for (int i=0; i<V; i++) 
-      G->vertices[i] = NULL;
+   G->adj = MATRIXint( V, V, 0);
    return G;
 }
 
-void criaVertice(int pos, int color, Graph G){
-    Vertex newVertice = (Vertex) malloc(sizeof(Vertex));
-    newVertice->cor = color;
-    newVertice->primeiroVizinho = NULL;
-    G->vertices[pos] = newVertice;
-
-    newVertice->numVertice = pos;
-
-}
 
 
-void criaAresta(int pos1, int pos2, Graph G){
-    if(pos1 >= G->V || pos2 >= G->V || pos1 < 0 || pos2 < 0)//posicao nao existe
-        return;
-    
-    
-    link vizinhoDo1 = (link) malloc(sizeof(link));
-    vizinhoDo1->w = G->vertices[pos2];
-    link vizinhoDo2 = (link) malloc(sizeof(link));
-    vizinhoDo2->w = G->vertices[pos1];
-
-    link aux = G->vertices[pos1]->primeiroVizinho;
-    G->vertices[pos1]->primeiroVizinho = vizinhoDo1;
-    vizinhoDo1->next = aux; 
-
-    aux = G->vertices[pos2]->primeiroVizinho;
-    G->vertices[pos2]->primeiroVizinho = vizinhoDo2;
-    vizinhoDo2->next = aux;  
-
-    G->A++;
-}
-
-void printGrafo(Graph G){
-    for(int i=0; i<G->V; i++){
-        printf("vertice %d de cor %d: ", i, G->vertices[i]->cor);
-        if(G->vertices[i] == NULL)//temporario
-            break;
-        for(link aux = G->vertices[i]->primeiroVizinho; aux!=NULL; aux = aux->next)
-            printf("vizinho %d, ", aux->w->numVertice);
-    printf("\n");
+void GRAPHinsertArc( Graph G, int v, int w) { 
+    if (G->adj[v][w] == 0) {
+        G->adj[v][w] = 1; 
+        G->A++;
     }
+   
 }
 
-void copiaVizinhos(int origem, int destino, Graph G){
-    link listaVizinhos = G->vertices[origem]->primeiroVizinho;
-    if(listaVizinhos == NULL) return;//nada a copiar
-    
-    link aux = G->vertices[destino]->primeiroVizinho;
-    while(aux->next != NULL) aux=aux->next;//pegando a ultima posição
-    aux->next = listaVizinhos;
-    // listaVizinhos->next = NULL;
+void GRAPHremoveArc( Graph G, int v, int w) { 
+   if (G->adj[v][w] == 1) {
+      G->adj[v][w] = 0; 
+      G->A--;
+   }
+}
+
+void GRAPHshow( Graph G) { 
+    for(int i=0; i<G->V; i++){
+        for(int j=0; j<G->V; j++){
+            printf("%d", G->adj[i][j]);
+        }
+        printf("\n");
+    }
+
+   for (int v = 0; v < G->V; ++v) {
+      printf( "%2d:", v);
+      for (int w = 0; w < G->V; ++w)
+         if (G->adj[v][w] == 1) 
+            printf( " %2d", w);
+      printf( "\n");
+   }
+}
+
+
+void insereAresta(Graph G, int numL, int numC){
+//loops separados para evitar dependência (otimização) 
+    for(int i=1; i<numL-1; i++){
+        for(int j=1; j<numC-1; j++){
+
+            GRAPHinsertArc(G, i*numC + j, i*numC + j + 1);//vizinho da direita
+            GRAPHinsertArc(G, i*numC + j, i*numC + j - 1);//vizinho da esquerda
+            GRAPHinsertArc(G, i*numC + j, (i-1)*numC + j);//vizinho de cima
+            GRAPHinsertArc(G, i*numC + j, (i+1)*numC + j);//vizinho de baixo
+        }
+    }
+    for(int j=1; j<numC-1; j++){//linha 0
+        GRAPHinsertArc(G, j, j + 1);//vizinho da direita
+        GRAPHinsertArc(G, j, j - 1);//vizinho da esquerda
+        GRAPHinsertArc(G, j, numC + j);//vizinho de baixo
+        //nao tem vizinho de cima
+        int i=numL-1;
+        GRAPHinsertArc(G, i*numC + j, i*numC + j + 1);//vizinho da direita
+        GRAPHinsertArc(G, i*numC + j, i*numC + j - 1);//vizinho da esquerda
+        GRAPHinsertArc(G, i*numC + j, (i-1)*numC + j);//vizinho de cima
+        //nao tem vizinho de baixo
+    }
+
+    for(int i=1; i<numL-1; i++){//coluna 0
+        GRAPHinsertArc(G, i*numC, i*numC + 1);//vizinho da direita
+        GRAPHinsertArc(G, i*numC, (i-1)*numC);//vizinho de cima
+        GRAPHinsertArc(G, i*numC, (i+1)*numC);//vizinho de baixo
+        //nao tem vizinho da esquerda
+        int j=numC-1;
+        GRAPHinsertArc(G, i*numC + j, i*numC + j - 1);//vizinho da esquerda
+        GRAPHinsertArc(G, i*numC + j, (i-1)*numC + j);//vizinho de cima
+        GRAPHinsertArc(G, i*numC + j, (i+1)*numC + j);//vizinho de baixo
+        //nao tem vizinho da direita
+    }
+
+    int i=0, j=0;//canto superior esquerdo
+    GRAPHinsertArc(G, 0, 1);//vizinho da direita
+    GRAPHinsertArc(G, 0, numC);//vizinho de baixo
+    //nao tem vizinho da esquerda nem de cima
+
+    i=0; j=numC-1;//canto superior direito
+    GRAPHinsertArc(G,  j, j - 1);//vizinho da esquerda
+    GRAPHinsertArc(G, j, numC + j);//vizinho de baixo
+    //nao tem vizinho de cima nem da direita
+
+    i=numL-1; j=0;//canto inferior esquerdo
+    GRAPHinsertArc(G, i*numC + j, i*numC + j + 1);//vizinho da direita
+    GRAPHinsertArc(G, i*numC + j, (i-1)*numC + j);//vizinho de cima
+    //nao tem vizinho de baixo nem da esquerda
+
+    i=numL-1; j=numC-1;//canto inferior direito
+    GRAPHinsertArc(G, i*numC + j, i*numC + j - 1);//vizinho da esquerda
+    GRAPHinsertArc(G, i*numC + j, (i-1)*numC + j);//vizinho de cima
+    //nao tem vizinho de baixo nem da direita
 
 }
+
 
 int main(){
-    // Graph G;
-    // G = criaGrafo(4);
-    // criaVertice(0, 2, G);
-    // criaVertice(1, 3, G);
-    // criaVertice(2, 4, G);
-    // criaVertice(3, 5, G);
-    // criaAresta(0, 1, G);
-    // criaAresta(0, 2, G);
-    // criaAresta(0, 3, G);
-    // criaAresta(1, 2, G);
+    Graph G;
 
     int numL, numC, numColor;
     scanf("%d %d %d", &numL, &numC, &numColor);
-    Graph G = criaGrafo(numL*numC);
     int matrix[numL][numC];
     for(int i=0; i<numL; i++)
         for(int j=0; j<numC; j++)
@@ -120,28 +146,26 @@ int main(){
         printf("\n");
     }
 
-    for(int i=0; i<numL; i++){
-        for(int j=0; j<numC; j++)
-            criaVertice(i*numC + j, matrix[i][j], G);
-    }
+    G = GRAPHinit(numL*numC);
+    insereAresta(G, numL, numC);
+
 
     for(int i=0; i<numL; i++){
         for(int j=0; j<numC; j++){
-            criaAresta(i*numC + j, i*numC + j + 1, G);//vizinho da direita
-            criaAresta(i*numC + j, (i-1)*numC + j, G);//vizinho de cima
+            if(j>0 && matrix[i][j] == matrix[i][j-1])
+                printf("aqui\n");
+            if(i>0 && matrix[i][j] == matrix[i-1][j])
+                printf("aqui\n");
+        
         }
     }
 
-    for(int i=0; i<numL; i++){
-        for(int j=0; j<numC; j++){
-            if(i+1<numL && matrix[i][j] == matrix[i+1][j])//vizinho de cima
-                copiaVizinhos((i+1)*numC + j,i*numC + j, G);
-            if(j-1>=0 && matrix[i][j] == matrix[i][j-1])//vizinho da esquerda
-                copiaVizinhos(i*numC + j - 1,i*numC + j, G);
-        }
+    for(int i=0; i<G->V; i++){
+        G->adj[1][i] = G->adj[1][i] || G->adj[0][i];
+        G->adj[i][1] = G->adj[i][1] || G->adj[i][0];
+        G->adj[i][0] = 0; 
+        G->adj[0][i] = 0; 
     }
-
-    printGrafo(G);
-
+    GRAPHshow(G);
     return 0;
 }
