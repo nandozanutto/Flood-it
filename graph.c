a3cbf29262b5f21641ca623c7a215dc22588f6bf
nandozanutto <nando.zanutto@gmail.com>
2021-08-01 22:07:03 -0300

    despinta done

--- graph.c
+++ graph.c
@@ -3,6 +3,8 @@
 
 typedef struct graph *Graph;
 typedef struct vertex *ApontaVertex;
+typedef struct mudanca *ApontaMudanca;
+typedef struct nodo *ApontaNodo;
 
 /* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A estrutura graph representa um grafo. O campo adj é um ponteiro para a matriz de adjacências do grafo. O campo V contém o número de vértices e o campo A contém o número de arcos do grafo. */
 
@@ -18,6 +20,18 @@ struct graph {
 struct vertex{
     int numVertice;
     int cor;
+};
+
+struct mudanca{
+    int raizAntiga;
+    int corDaRaizAntiga;
+    ApontaNodo listaMudanca;
+
+};
+
+struct nodo{
+    int *mudou;
+    ApontaNodo proximo;
 };
 
 /* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHinit() constrói um grafo com vértices 0 1 .. V-1 e nenhum arco. */
@@ -59,12 +73,12 @@ void GRAPHremoveArc( Graph G, int v, int w) {
 }
 
 void GRAPHshow( Graph G) { 
-    for(int i=0; i<G->V; i++){
-        for(int j=0; j<G->V; j++){
-            printf("%d", G->adj[i][j]);
-        }
-        printf("\n");
-    }
+    // for(int i=0; i<G->V; i++){
+    //     for(int j=0; j<G->V; j++){
+    //         printf("%d", G->adj[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
    for (int v = 0; v < G->V; ++v) {
       printf( "%2d(color %d excluido %d): ", v, G->adj[v][G->V], G->adj[v][G->V+1]);
@@ -75,6 +89,26 @@ void GRAPHshow( Graph G) {
    }
 }
 
+ApontaMudanca criaMudanca(){
+    ApontaMudanca mudanca = (ApontaMudanca) malloc(sizeof(struct mudanca));
+    mudanca->listaMudanca = NULL;
+    return mudanca;
+}
+
+void adicionaMudanca(ApontaMudanca mudanca, int * mudou){
+    if(mudanca->listaMudanca == NULL){
+        mudanca->listaMudanca = (ApontaNodo) malloc(sizeof(struct nodo));
+        mudanca->listaMudanca->proximo = NULL;
+        mudanca->listaMudanca->mudou = mudou;
+    }
+    else{
+        ApontaNodo aux = mudanca->listaMudanca;
+        ApontaNodo newNodo = (ApontaNodo) malloc(sizeof(struct nodo));
+        newNodo->proximo = aux;
+        newNodo->mudou = mudou;
+        mudanca->listaMudanca = newNodo;//novo nodo passa ser cabeça
+    }
+}
 
 void lerArestas(Graph G, int numL, int numC){
 //loops separados para evitar dependência (otimização) 
@@ -149,17 +183,73 @@ void copiaVizinhos(int o, int d, Graph G){
 
 }
 
+void copiaVizinhos2(int o, int d, Graph G, ApontaMudanca mudanca){
+    if(G->adj[o][G->V+1]) return;//o já foi removido!!!!
+    if(o == G->raiz)//raiz foi removida por d
+        G->raiz = d;
+    for(int i=0; i<G->V; i++){
+        int valorAntigo = G->adj[d][i];
+        G->adj[d][i] = G->adj[d][i] || G->adj[o][i];//copiando a vizinhança o para d
+        if(G->adj[d][i] != valorAntigo)//este valor mudou
+            adicionaMudanca(mudanca, G->adj[d] + i);
 
+        valorAntigo = G->adj[i][d];
+        G->adj[i][d] = G->adj[i][d] || G->adj[i][o];//faz os vizinhos de o apontarem pra d 
+        if(G->adj[i][d] != valorAntigo)//este valor mudou
+            adicionaMudanca(mudanca, G->adj[i] + d);
 
-void pintaVertice(Graph G, int v, int cor){
+        // G->adj[i][o] = 0;//zera os arcos para o
+        // G->adj[o][i] = 0;//zero os arcos de o
+        G->adj[i][i] = 0;//exclui possível aresta pro mesmo nodo
+    }
+    G->adj[o][G->V+1] = 1;//mata vertice
+    adicionaMudanca(mudanca, G->adj[o] + G->V+1);//este valor mudou
+
+    G->numExcluidos++;
+
+}
+
+void GRAPHshow2( Graph G) { 
+    // for(int i=0; i<G->V; i++){
+    //     for(int j=0; j<G->V; j++){
+    //         printf("%d", G->adj[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+   for (int v = 0; v < G->V; ++v) {
+      printf( "%2d(color %d excluido %d): ", v, G->adj[v][G->V], G->adj[v][G->V+1]);
+      if(G->adj[v][G->V+1]){ 
+        printf( "\n");
+        continue;//v foi excluido
+      }
+      for (int w = 0; w < G->V; ++w)
+         if (G->adj[v][w] == 1 && !G->adj[w][G->V+1])//se é vizinho E tá vivo 
+            printf( " %2d", w);
+      printf( "\n");
+   }
+}
+
+void pintaVertice(Graph G, int v, int cor, ApontaMudanca mudanca){ 
+    mudanca->raizAntiga = G->raiz;//salvando raiz
     if(v != G->raiz)//só pode pintar a raiz
         return;
+    mudanca->corDaRaizAntiga = G->adj[v][G->V];//salvando a cor da raiz
     G->adj[v][G->V] = cor;
     //necessário checar se ao mudar de cor o vértice junta com algum vizinho
     for(int i=0; i<G->V; i++){
         if(i != v && G->adj[v][i] && G->adj[i][G->V] == cor){//se é vizinho de v e tem a mesma cor
-            copiaVizinhos(i, v, G);//mato vizinho
+            copiaVizinhos2(i, v, G, mudanca);//mato vizinho
         }
+    }
+}
+
+void despinta(Graph G, ApontaMudanca mudanca){
+    //revertendo mudanças feitas
+    G->raiz = mudanca->raizAntiga;
+    G->adj[G->raiz][G->V] = mudanca->corDaRaizAntiga;
+    for(ApontaNodo aux=mudanca->listaMudanca; aux!=NULL; aux=aux->proximo){
+        *aux->mudou = (*aux->mudou + 1)%2;//inverte valor 0 -> 1 1 -> 0 
     }
 }
 
@@ -207,6 +297,7 @@ int main(){
 
 
     // GRAPHshow(G);
+    printf("raiz eh %d\n", G->raiz);
     // pintaVertice(G, G->raiz, 3);
     // pintaVertice(G, G->raiz, 1);
     // pintaVertice(G, G->raiz, 3);
@@ -221,22 +312,37 @@ int main(){
     // pintaVertice(G, G->raiz, 1);
     // printf("raiz eh %d\n", G->raiz);
 
+    ApontaMudanca mudanca = criaMudanca();
     int numPaint=0;
     int corPaint;
-    // GRAPHshow(G);
+    GRAPHshow(G);
     scanf("%d", &numPaint);
     for(int i=0; i<numPaint; i++){
-        // GRAPHshow(G);
+        GRAPHshow2(G);
         scanf("%d", &corPaint);
-        pintaVertice(G, G->raiz, corPaint);
+        pintaVertice(G, G->raiz, corPaint, mudanca);
         // printf("raiz eh %d\n", G->raiz);
+        printf("******************\n");
     }
 
 
+    // printf("**********antes de pintar\n");
+    // GRAPHshow2(G);
+    // pintaVertice(G, G->raiz, 3, mudanca);
+    // printf("**********depois de pintar\n");
+    // GRAPHshow2(G);
+    // despinta(G, mudanca);
+    // printf("**********depois de DESpintar\n");
+    // GRAPHshow2(G);
 
+    // G->adj[G->V-1][G->V] = 4;
+    // int * teste = G->adj[G->V-1] + G->V;
+    // *teste = 6;
 
-    // GRAPHshow(G);
+    GRAPHshow2(G);
     printf("excluidos %d raiz %d\n", G->numExcluidos, G->raiz);
+
+
 
     return 0;
 }
--- test/script.sh
+++ test/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "testando...."
-./geramapa 50 50 5 > input.txt
+./geramapa 20 20 3 > input.txt
 ./floodit_h1 < input.txt > output.txt
 cat input.txt > ../test.txt 
 cat output.txt >> ../test.txt
